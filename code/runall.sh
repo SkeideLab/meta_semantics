9e73f5eb9cf22c338e8de60398787b8c45ea8409
Alexander <alexander.enge@hu-berlin.de>
2021-03-01 19:25:07 +0100

    update clusterrun pwd

--- code/runall.sh
+++ code/runall.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-cd /home/mask_children/code
-. /opt/miniconda-latest/etc/profile.d/conda.sh
-conda activate nimare
 JUPYTER_PARAMS=(--to=notebook --execute --inplace \
     --ExecutePreprocessor.kernel_name=python3 \
     --ExecutePreprocessor.timeout=-1)
 jupyter nbconvert "${JUPYTER_PARAMS[@]}" nb01_ale.ipynb
-jupyter nbconvert "${JUPYTER_PARAMS[@]}" nb04_sdm.ipynb
+#jupyter nbconvert "${JUPYTER_PARAMS[@]}" nb04_sdm.ipynb
