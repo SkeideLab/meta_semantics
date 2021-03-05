138f9a9c9e82466bf0fd68257600ff24321cc992
Alexander Enge <alexander.enge@hu-berlin.de>
2021-03-05 10:14:30 +0000

    start the generate_null function

--- code/nb06_fsn.py
+++ code/nb06_fsn.py
@@ -10,5 +10,36 @@
 # ---
 
 # %%
-3 + 3
+
+from nimare import io, utils
+import numpy as np
+import pandas as pd
+from nilearn import image, plotting
+
+# %%
+
+# Read table of experiments from ALE analysis
+exps = pd.read_json("../results/exps.json")
+exps["foci"] = [np.array(foci, dtype="float") for foci in exps["foci"]]
+
+# %%
+
+# Load ALE template
+temp = utils.get_template("ale_2mm", mask="gm")
+
+# Extract MNI coordinates of all gray matter voxels
+x, y, z = np.where(temp.get_fdata() == 1.0)
+within_mni = image.coord_transform(x=x, y=y, z=z, affine=temp.affine)
+within_mni = np.array(within_mni)
+
+
+within_mni[:,0]
+
+# %%
+
+k = 10
+
+dset = io.convert_sleuth_to_dataset("../results/ale/all.txt")
+ns = dset.metadata['sample_sizes'].apply(lambda x: x[0])
+
 
