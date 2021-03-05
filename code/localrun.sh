e9c6291b309beea04d386e4f7cfc86d23e021942
Alexander <alexander.enge@hu-berlin.de>
2021-03-05 11:12:06 +0100

    make the hpc run parallel

--- code/localrun.sh
+++ code/localrun.sh
@@ -7,5 +7,7 @@
 # jupyter nbconvert "${JUPYTER_PARAMS[@]}" nb04_sdm.ipynb
 
 python3 nb01_ale.py
+python3 nb02_subtraction.py
+python3 nb03_adults.py
 #python3 nb04_sdm.py
-python3 nb05_jackknife.py
+#python3 nb05_jackknife.py
