ed3d53a89e568e30f9a100f78246b7a55dfe219f
Alexander Enge <alexander.enge@hu-berlin.de>
2021-03-06 12:09:03 +0000

    finish algorithm for a single cluster

--- code/nb06_fsn.py
+++ code/nb06_fsn.py
@@ -11,38 +11,29 @@
 
 # %%
 
-from nimare.utils import get_template
-import numpy as np
-from nilearn import image, regions, plotting
-from os import makedirs, path
-import random
-from re import sub
+# Define function to generate a new data set with k null studies added
+def generate_null(
+    text_file="foci.txt",
+    space="ale_2mm",
+    k_null=200,
+    random_seed=None,
+    output_dir="./",
+):
 
-# %%
-
-# Load ALE template
-temp = get_template("ale_2mm", mask="gm")
-
-# Extract MNI coordinates of all gray matter voxels
-x, y, z = np.where(temp.get_fdata() == 1.0)
-within_mni = image.coord_transform(x=x, y=y, z=z, affine=temp.affine)
-within_mni = np.array(within_mni).transpose()
-
-# Backup this list of coordinates
-makedirs("../results/fsn/", exist_ok=True)
-np.savetxt("../results/fsn/within_ale_2mm.txt", within_mni, fmt="%.0f")
-
-# %%
-
-# Define function to generate a new Sleuth with k null studies added
-def write_null(text_file, k_null, random_seed, output_dir):
-
-    from nimare import io
+    from nimare import io, utils
     import numpy as np
     from os import makedirs, path
     import random
     from re import sub
     from shutil import copy
+
+    # Load NiMARE's gray matter template
+    temp = utils.get_template(space=space, mask="gm")
+
+    # Extract possible MNI coordinates for all gray matter voxels
+    x, y, z = np.where(temp.get_fdata() == 1.0)
+    within_mni = image.coord_transform(x=x, y=y, z=z, affine=temp.affine)
+    within_mni = np.array(within_mni).transpose()
 
     # Read the original Sleuth file into a NiMARE data set
     dset = io.convert_sleuth_to_dataset(text_file, target="ale_2mm")
@@ -52,16 +43,16 @@ def write_null(text_file, k_null, random_seed, output_dir):
         random.seed(random_seed)
 
     # Resample numbers of subjects per experiment based on the original data
-    no_subjects_dset = [n[0] for n in dset.metadata["sample_sizes"]]
-    no_subjects_null = random.choices(no_subjects_dset, k=k_null)
+    nr_subjects_dset = [n[0] for n in dset.metadata["sample_sizes"]]
+    nr_subjects_null = random.choices(nr_subjects_dset, k=k_null)
 
-    # Do the same for the number of foci per experiment
-    no_foci_dset = dset.coordinates["study_id"].value_counts().tolist()
-    no_foci_null = random.choices(no_foci_dset, k=k_null)
+    # Do the same for the numbers of foci per experiment
+    nr_foci_dset = dset.coordinates["study_id"].value_counts().tolist()
+    nr_foci_null = random.choices(nr_foci_dset, k=k_null)
 
     # Create random foci
     idx_list = [
-        random.sample(range(len(within_mni)), k=k_foci) for k_foci in no_foci_null
+        random.sample(range(len(within_mni)), k=k_foci) for k_foci in nr_foci_null
     ]
     foci_null = [within_mni[idx] for idx in idx_list]
 
@@ -69,30 +60,26 @@ def write_null(text_file, k_null, random_seed, output_dir):
     makedirs(output_dir, exist_ok=True)
     text_file_basename = os.path.basename(text_file)
     null_file_basename = sub(
-        ".txt", "_plus_k" + str(k_null) + ".txt", string=text_file_basename
+        pattern=".txt", repl="_plus_k" + str(k_null) + ".txt", string=text_file_basename
     )
     null_file = output_dir + "/" + null_file_basename
     copy(text_file, null_file)
 
-    # Open this new file
+    # Write all the null studies to this file
     f = open(null_file, mode="a")
-
-    # Add all the null studies to this file
     for i in range(k_null):
         f.write(
             "\n// nullstudy"
             + str(i + 1)
             + "\n// Subjects="
-            + str(no_subjects_null[i])
+            + str(nr_subjects_null[i])
             + "\n"
         )
         np.savetxt(f, foci_null[i], fmt="%.3f", delimiter="\t")
-
-    # Close the file
     f.close()
 
-    # Return the new Sleuth file and return it as a NiMARE data set
-    dset_null = io.convert_sleuth_to_dataset(null_file, target="ale_2mm")
+    # Read the file and return it as a NiMARE data set
+    dset_null = io.convert_sleuth_to_dataset(null_file, target=space)
     return dset_null
 
 
@@ -100,74 +87,101 @@ def write_null(text_file, k_null, random_seed, output_dir):
 
 # Params
 text_file = "../results/ale/all.txt"
+space = "ale_2mm"
 voxel_thresh = 0.001
 cluster_thresh = 0.01
 n_iters = 1000
-k_null_max = 100
+k_null_max = 200
+random_seed = 1234
+output_dir = "../results/fsn/all"
 
 from nimare import io, meta, correct
-from nilearn import regions
+from nilearn import regions, image
 from scipy.stats import norm
+import numpy as np
+
+# Specify ALE and FWE transformers
+ale = meta.cbma.ALE()
+corr = correct.FWECorrector("montecarlo", voxel_thresh=voxel_thresh, n_iters=n_iters)
 
 # Perform ALE analysis for the original Sleuth file
-dset = io.convert_sleuth_to_dataset(text_file=text_file, target="ale_2mm")
-ale = meta.cbma.ALE()
-res = ale.fit(dset)
-corr = correct.FWECorrector("montecarlo", voxel_thresh=voxel_thresh, n_iters=n_iters)
-cres = corr.transform(result=res)
+dset_orig = io.convert_sleuth_to_dataset(text_file=text_file, target=space)
+res_orig = ale.fit(dset_orig)
+cres_orig = corr.transform(result=res_orig)
+
+# Store a list of the study IDs of the original studies
+study_ids_orig = dset_orig.metadata["id"].tolist()
 
 # Extract thresholded cluster image
-cluster_thresh_z = threshold = norm.ppf(1 - 0.01 / 2)
+cluster_thresh_z = norm.ppf(1 - cluster_thresh / 2)
 
 # Create separate maps for all significant clusters
-img_orig = cres.get_map("z_level-cluster_corr-FWE_method-montecarlo")
+img_orig = cres_orig.get_map("z_level-cluster_corr-FWE_method-montecarlo")
 img_orig = image.threshold_img(img_orig, threshold=cluster_thresh_z)
 imgs_orig, _ = regions.connected_regions(img_orig, min_region_size=0)
 
 # %%
 
+# Create a new data set with null studies added
+dset_null = generate_null(
+    text_file=text_file,
+    space=space,
+    k_null=k_null_max,
+    random_seed=random_seed,
+    output_dir=output_dir,
+)
+
+# %%
+
 # # Loop over clusters
-# for i in range(imgs_clusters_orig.shape[3]):
+# for cl in range(imgs_clusters_orig.shape[3]):
 
-k_null_max = 200
+# Current cluster
+cl = 0
 
-i = 2
+# Extract the map with only the current cluster
+img_cluster = image.index_img(imgs_orig, index=cl)
 
-img_region = image.index_img(imgs_orig, index=i)
-
+# Create lists to keep track of the sampleing process:
+# 1) How many null studies did we use at this step?
 list_k = np.array([0, k_null_max], dtype="int")
+# 2) Was our cluster at this step significant or not?
 list_sig = np.array([True, False], dtype="bool")
 
+# Start sampling
 while True:
 
+    # Compute how many null studies to use for the current iteration
     current_k = np.mean([list_k[list_sig].max(), list_k[~list_sig].min()], dtype="int")
 
-    # # Compute new ALE with the maximum number of null studies added
-    # dset_null = write_null(
-    #     text_file=text_file,
-    #     k_null=current_k,
-    #     random_seed=1234,
-    #     output_dir="../results/fsn/all",
-    # )
-    # res_null = res = ale.fit(dset_null)
-    # cres_null = corr.transform(result=res_null)
+    # Create a list of these null studies
+    study_ids_null = ["nullstudy" + str(x + 1) + "-" for x in range(current_k)]
 
-    # # Check if the cluster remained significant
-    # img_null = cres_null.get_map("z_level-cluster_corr-FWE_method-montecarlo")
-    # img_null = image.threshold_img(img_null, threshold=cluster_thresh_z)
-    # img_check = image.math_img("img1 * img2", img1=img_region, img2=img_null)
-    # region_significant = np.any(img_check.get_fdata())
+    # Create a new data set where these are added to the original studies
+    study_ids = study_ids_orig + study_ids_null
+    dset_current = dset_null.slice(study_ids)
 
-    region_significant = current_k < 2
+    # Compute the ALE for the current data set
+    res_current = res = ale.fit(dset_current)
+    cres_current = corr.transform(result=res_current)
 
+    # Check if the cluster remained significant
+    img_current = cres_current.get_map("z_level-cluster_corr-FWE_method-montecarlo")
+    img_current = image.threshold_img(img_current, threshold=cluster_thresh_z)
+    img_check = image.math_img("img1 * img2", img1=img_cluster, img2=img_current)
+    cluster_significant = np.any(img_check.get_fdata())
+
+    # Add the new information to the lists
     list_k = np.append(list_k, current_k)
-    list_sig = np.append(list_sig, region_significant)
+    list_sig = np.append(list_sig, cluster_significant)
 
-    # Break as soon as we're sampling the same value twice
+    # Stop as soon as k doesn't change animore. We've found our FSN!
     if len(np.unique(list_k)) != len(list_k):
         break
 
+# Take a look at the sampling process
 print(list_k)
+print(list_sig)
 
 # %%
 
