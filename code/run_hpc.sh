fb96bc6fec6152037de4c77e88ebd16a49620b0f
Alexander <alexander.enge@hu-berlin.de>
2021-07-12 07:56:14 +0200

    reformat batch script

--- code/run_hpc.sh
+++ code/run_hpc.sh
@@ -34,10 +34,10 @@ module load singularity
 # Create shorthand command for executing scripts inside the container
 EXEC=(
     srun -N1 -n1 singularity exec
-        --bind /ptmp/aenge/meta_semantics:/home/jovyan/meta_semantics
-        --home /ptmp/aenge/home
-        --pwd /home/jovyan/meta_semantics/code
-        meta_semantics_latest.sif
+    --bind /ptmp/aenge/meta_semantics:/home/jovyan/meta_semantics
+    --home /ptmp/aenge/home
+    --pwd /home/jovyan/meta_semantics/code
+    meta_semantics_latest.sif
 )
 
 # First, perform only the actual ALE analyses
@@ -45,16 +45,16 @@ EXEC=(
 
 # Then, perform all of the other analyses in parallel
 "${EXEC[@]}" python3 nb02_subtraction.py &
-    "${EXEC[@]}" python3 nb03_adults.py &
-    "${EXEC[@]}" python3 nb04_sdm.py &
-    "${EXEC[@]}" python3 nb05_jackknife.py &
-    "${EXEC[@]}" python3 nb06_fsn.py all 4 &
-    "${EXEC[@]}" python3 nb06_fsn.py all 3 &
-    "${EXEC[@]}" python3 nb06_fsn.py all 3 &
-    "${EXEC[@]}" python3 nb06_fsn.py knowledge,relatedness,objects 10 &
-    "${EXEC[@]}" python3 nb09_supplement.py
+"${EXEC[@]}" python3 nb03_adults.py &
+"${EXEC[@]}" python3 nb04_sdm.py &
+"${EXEC[@]}" python3 nb05_jackknife.py &
+"${EXEC[@]}" python3 nb06_fsn.py all 4 &
+"${EXEC[@]}" python3 nb06_fsn.py all 3 &
+"${EXEC[@]}" python3 nb06_fsn.py all 3 &
+"${EXEC[@]}" python3 nb06_fsn.py knowledge,relatedness,objects 10 &
+"${EXEC[@]}" python3 nb09_supplement.py
 wait
 
 # Create output tables and figures
 "${EXEC[@]}" python3 nb07_tables.py &
-    "${EXEC[@]}" python3 nb08_figures.py
+"${EXEC[@]}" python3 nb08_figures.py
